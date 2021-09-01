9b692e0eac60f64fab3d8dabc1596b5480c90585
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-01 21:07:04 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,15 @@
-export default fetchCountries;
+// const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+// const fetchCountries = searchQuery => {
+//   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => response.json());
+// };
+
+// export default { fetchCountries };
+
+//  vlad
+
+function fetchCountries(searchQuery) {
+  fetch(`https://restcountries.eu/rest/v2/name/{searchQuery}? fields={field};{field};{field}
+https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
+`);
+}
