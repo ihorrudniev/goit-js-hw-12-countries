8024a6459da5c2d2d3a4f84881bacf1950f218d7
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-02 20:53:30 +0300

    files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './sass/main.scss';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
+import debounce from 'lodash.debounce';
+
+import templatesCountry from './templates/templatesCountry.hbs';
 
 alert({
   text: 'Notice me, senpai!',
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,8 @@
-// const BASE_URL = 'https://restcountries.eu/rest/v2';
-
-// const fetchCountries = searchQuery => {
-//   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => response.json());
-// };
-
-// export default { fetchCountries };
-
-//  vlad
-
-function fetchCountries(searchQuery) {
-  fetch(`https://restcountries.eu/rest/v2/name/{searchQuery}? fields={field};{field};{field}
+const BASE_URL = function fetchCountries(searchQuery) {
+  fetch(`https://restcountries.eu/rest/v2/name/{searchQuery}?fields={field};{field};{field}
 https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
-`);
-}
+`)
+    .then(response => response.json())
+    .catch(err => console.log(err));
+};
+export default { fetchCountries };
