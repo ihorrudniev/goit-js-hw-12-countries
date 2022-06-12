a89f5eb41b1f4c36e5ec24a72701609f5a3ca2e4
Ihor Rudniev <rudnievihor@gmail.com>
2022-06-12 20:45:04 +0300

    fix-url

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+const BASE_URL = 'https://restcountries.com/v2/';
 
 function fetchCountries(searchQuery) {
   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => response.json());
